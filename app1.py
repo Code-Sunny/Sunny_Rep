b11f26475673ec59008342094665e4c59a5baa56
LinQuixote <96329698+LinQuixote@users.noreply.github.com>
2022-03-09 10:23:00 +0900

    singer_name, id, img, preview
    api를 통해 가수의 이름. 곡명, 앨범 이미지, 미리듣기 정보 가져오기

--- app1.py
+++ app1.py
@@ -1,3 +1,4 @@
+from typing import Any
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pprint
@@ -7,7 +8,9 @@ secret = ''
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
  
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-# pprint.pprint()
+
+# 여기까지는 기본 설정~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 # spotify안에 있는 카테고리들 출력해보기
 info_cate = sp.categories(country=None, locale=None, limit=20, offset=0)
@@ -17,43 +20,51 @@ info_cate = sp.categories(country=None, locale=None, limit=20, offset=0)
 top_id = info_cate['categories']['items'][0]['id']
 
 
-# # 첫번째는 카테의 정보인데 걍 쩌리, 두번째는 카테 안의 리스트
+# 카테 안의 플레이리스트의 아이디들을 추출
 # cate_info = sp.category( top_id , country=None , locale=None )
-cate_list = sp.category_playlists( category_id= top_id, country=None , limit=5 , offset=0 )
+cate_list = sp.category_playlists( 
+category_id= top_id, country=None , limit=5 , offset=0 )
 # pprint.pprint(cate_list)
 
-# # id들을 담을 리스트 생성!
+# # 플레이리스트 id들을 담을 리스트 생성!
 ids = []
 
-# id들을 꺼내기 위한 준비, 리스트에 담긴 상태로 놓고, 인덱스 하나씩 열어서 꺼내버리기
+# 플레이리스트 id들을 꺼내기 위한 준비, 리스트에 담긴 상태로 놓고
 dirty_ids = cate_list['playlists']['items']
-# pprint.pprint(dirty_ids)
-for i in dirty_ids:
-    # print(i)
-    # print('~'*100)
 
+# 인덱스 하나씩 열어서 플레이리스트 아이디를 ids 리스트에 담기
+for i in dirty_ids:
     single_id = i['id'] 
     ids.append(single_id)
-    # print('~'*100)
-    # print(single_id)
-# pprint.pprint(first)
-# print(ids)
+
+# 플레이리스트id를 통해 가수 id 추출하기
+# 추출한 id는 final 리스트에 담기
 final = []
 for i in ids:
     tracks = sp.playlist_items(i, fields=None , limit=1 , offset=0 , market=None , additional_types=('track' , 'episode'))
     k = tracks['items'][0]['track']['artists'][0]['id']
     final.append(k)
-    # print(k)
-    # print('~'*100)
 
-# print(final)
+# 추출한 가수 id를 통해서 가수, 가수의 노래들, 노래이미지, 노래 미리듣기 추출
 for i in final:
     name = sp.artist(i)
     real_name = name['name']
     real_id = name['id']
+    sings = sp.artist_albums(real_id, album_type=None, country=None, limit=1, offset=0)
+    sing_name = sings['items'][0]['name']
+    sing_pic = sings['items'][0]['images'][0]['url']
+    sing_id = sings['items'][0]['id']
+    
     print(real_name)
     print(real_id)
-    # k = tracks['items'][0]
-    # final.append(k)
+    print(sing_name)
+    print(sing_pic)
+
+    track = sp.album_tracks(sing_id, limit=1, offset=0, market=None)
+    sing_pre = track['items'][0]['preview_url']
+    print(sing_pre)
+
     print('~'*100)
+    
+
 
