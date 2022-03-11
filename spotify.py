9a5b405afc72585e27d2c8a04b3cf53682587d6d
LucetTin5 <lucettin5@gmail.com>
2022-03-11 14:04:32 +0900

    search making, play test

--- spotify.py
+++ spotify.py
@@ -18,10 +18,17 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 # genres
 # response = sp.recommendation_genre_seeds()["genres"]
 # print(len(response), response)
-
+query = "아이유"
 # search
-response = sp.search("artist:IU", limit=1, type="track")
+response = sp.search(query, limit=10, type="track")
 # items = response["tracks"]["items"]
 
-with open("test.py", "w") as file:
-    file.write(str(response))
+print(response)
+
+
+def get_songs(query_type, query):
+    if query_type == "artist":
+        query = "artist:" + query
+    else:
+        query = "track:" + query
+    response = sp.search(query, limit=20, type="track")
